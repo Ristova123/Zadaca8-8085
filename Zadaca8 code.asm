9c8db248fc95023d11dd91f8d0af9f5b18550822
Tamara Ristova <tamararistova11@hotmail.com>
2024-05-31 22:26:44 +0200

    Create Zadaca8 code.asm

---
+++ Zadaca8 code.asm
@@ -0,0 +1,58 @@
+
+MVI A.0 ;
+
+ STA RDN_BR
+
+ MVI E,255d
+
+VRTI: MOV A,E ;
+
+ ANI FFh
+
+ JNZ VRTI ;
+
+ HLT ;
+
+ END
+
+ RDN_BR DS 1 ;
+
+2Ch: CALL SERVIS_55 ;
+
+ RET
+
+34h: CALL SERVIS_65 ;
+
+ RET
+
+SERVIS_55: MVI A,1 ;
+
+ OUT 01h
+
+ LDA RDN_BR ;
+
+INR A ;
+
+ STA RDN_BR
+
+ OUT 02h
+
+ DCR E ;
+
+ RET
+
+SERVIS_65: MVI A,2d ;
+
+ OUT 01h
+
+ LDA RDN_BR ;
+
+ INR A ;
+
+ STA RDN_BR
+
+ OUT 02h
+
+ DCR E ;
+
+RET
